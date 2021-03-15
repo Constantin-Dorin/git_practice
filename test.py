c54d1479465760f048327586eb185bd1c2b6424d
unknown <constantin-dorin.nicolescu@outlook.com>
2021-03-15 17:04:30 +0200

    App

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-#This is a practice file
+def delete_starting_evens(lst):
+    elemente = range(len(lst))
+    output = []
+    for index in elemente:
+        if lst[index] % 2 == 0:
+            continue
+        else:
+            output = lst[index:]
+            break
+    return output
+
+
+lista = [4, 8, 10, 11, 12, 15]
+
+print(delete_starting_evens(lista))
